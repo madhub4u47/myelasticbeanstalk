24d24373d7bba5412aaa8d3909edf824c30d6d6d
madhu <madhusudhan.g47@gmail.com>
2019-08-17 15:09:13 -0500

    initial commit

---
+++ src/main/java/com/aws/ElasticBeanStalk/ElasticBeanStalkApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ElasticBeanStalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticBeanStalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticBeanStalkApplication.class, args);
+	}
+
+}
